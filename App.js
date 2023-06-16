edbfbb2f32da2882104558c32949885a9639b3b7
Ranjith-Sixt <ranjith.r@sixt.com>
2023-06-16 20:59:14 +0530

    Episode-01

---
+++ App.js
@@ -0,0 +1,14 @@
+const header = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child 1" }, [
+        React.createElement("h1", {}, "hello 1"),
+        React.createElement("h1", {}, "hello 2")
+    ]),
+    React.createElement("div", { id: "child 2" }, [
+        React.createElement("h1", {}, "hello 1"),
+        React.createElement("h1", {}, "hello 2")
+    ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(header);
